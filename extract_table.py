462b4df5c3b8ff6346235ec453426e736ca01c84
Prakash Adhikari <adhikariprakash007@gmail.com>
2025-03-26 11:45:18 +0000

    change feature in extract_table.py and solve bug in final.ipynb

--- extract_table.py
+++ extract_table.py
@@ -1,4 +1,4 @@
-from clean_html import call_build_block
+"""from clean_html import call_build_block
 from bs4 import BeautifulSoup
 import pandas as pd
 
@@ -53,5 +53,55 @@ def create_dataframes():
        
         dataframes.append(df)
 
-    return dataframes
+    return dataframes"""
+
+
+from clean_html import call_build_block
+from bs4 import BeautifulSoup
+import pandas as pd
+
+def create_dataframes():
+    html_data = call_build_block()
+    soup = BeautifulSoup(html_data, "html.parser")
+    tables = soup.find_all("table")
+
+    thead_dataframes = []
+    tbody_dataframes = []
+
+    for table in tables:
+        thead_data = []
+        tbody_data = []
+
+        thead = table.find("thead")
+        if thead:
+            for row in thead.find_all("tr"):
+                cols = row.find_all(["th", "td"])
+                thead_data.append([col.get_text(strip=True) for col in cols])
+
+        tbody = table.find("tbody")
+        if tbody:
+            orphan_ths = tbody.find_all("th", recursive=False)  
+            if orphan_ths:
+                for orphan_th in orphan_ths:
+                    new_td = soup.new_tag("td")
+                    new_td.string = orphan_th.get_text(strip=True)
+                    new_tr = soup.new_tag("tr")
+                    new_tr.append(new_td)
+                    orphan_th.insert_before(new_tr)
+                    orphan_th.decompose()
+            
+            for row in tbody.find_all("tr"):
+                cols = row.find_all(["th", "td"])
+                tbody_data.append([col.get_text(strip=True) for col in cols])
+
+        max_thead_cols = max((len(row) for row in thead_data), default=0)
+        max_tbody_cols = max((len(row) for row in tbody_data), default=0)
+        
+        thead_df = pd.DataFrame([row + [""] * (max_thead_cols - len(row)) for row in thead_data])
+        tbody_df = pd.DataFrame([row + [""] * (max_tbody_cols - len(row)) for row in tbody_data])
+        
+        thead_dataframes.append(thead_df)
+        tbody_dataframes.append(tbody_df)
+
+    return thead_dataframes, tbody_dataframes
 
